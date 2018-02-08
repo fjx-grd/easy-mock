8f36d5d70cd3575f360d4c1cb7993fd351506897
gzfengjianxin <gzfengjianxin@corp.netease.com>
2018-02-08 16:16:19 +0800

    update

--- app/router.js
+++ app/router.js
@@ -6,4 +6,11 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/*', controller.home.index);
+  router.post('/*', controller.home.index);
+  router.head('/*', controller.home.index);
+  router.options('/*', controller.home.index);
+  router.put('/*', controller.home.index);
+  router.patch('/*', controller.home.index);
+  router.del('/*', controller.home.index);
+  router.redirect('/*', controller.home.index);
 };
